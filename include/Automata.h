4ab0d668ff776b02953f1208f74c1db54c3bf628
den9995 <den9995@gmail.com>
2023-03-25 21:51:29 +0300

    indents

--- include/Automata.h
+++ include/Automata.h
@@ -1,39 +1,40 @@
+//Copyright 2023 den9995
 #ifndef INCLUDE_AUTOMATA_H_
 #define INCLUDE_AUTOMATA_H_
 #include <vector>
 #include <string>
 class Automata {
-   public:
-      enum State {
-         OFF,
-         WAIT,
-         ACCEPT,
-         CHECK,
-         COOK
-      };
-      
-   private:
-      long int cash;
-      std::vector<std::string> menu;
-      std::vector<int> prices;
-      std::vector<int> longitudes;
-      State state;
-      int menuEntry;
-   
-   public:
-      Automata();
-      void on();
-      void off();
-      void coin(int);
-      void getMenu();
-      State getState();
-      void choice(int);
-      void cancel();
-   
-   private:
-      void check(int);
-      void cook(int);
-      void finish(int);
+  public:
+    enum State {
+      OFF,
+      WAIT,
+      ACCEPT,
+      CHECK,
+      COOK
+    };
+     
+  private:
+    long int cash;
+    std::vector<std::string> menu;
+    std::vector<int> prices;
+    std::vector<int> longitudes;
+    State state;
+    int menuEntry;
+  
+  public:
+    Automata();
+    void on();
+    void off();
+    void coin(int);
+    void getMenu();
+    State getState();
+    void choice(int);
+    void cancel();
+  
+  private:
+    void check(int);
+    void cook(int);
+    void finish(int);
 };
 
-#endif
+#endif  // INCLUDE_AUTOMATA_H_
--- src/Automata.cpp
+++ src/Automata.cpp
@@ -1,72 +1,73 @@
+//Copyright 2023 den9995
 #include "Automata.h"
 #include <iostream>
 #include <string>
 #ifdef _WIN32
-   #include <Windows.h>
+  #include <Windows.h>
 #else
-   #include <unistd.h>
+  #include <unistd.h>
 #endif
 Automata::Automata() {
-   menu = {"кипяток", "кофе", "чай"};
-   prices = {12, 51, 34};
-   longitudes = {2, 3, 1};
-   state = OFF;
-   cash = 0;
+  menu = {"кипяток", "кофе", "чай"};
+  prices = {12, 51, 34};
+  longitudes = {2, 3, 1};
+  state = OFF;
+  cash = 0;
 }
 void Automata::on() {
-   if (state == OFF) state = WAIT;
+  if (state == OFF) state = WAIT;
 }
 void Automata::off() {
-   if (state == WAIT) state = OFF;
+  if (state == WAIT) state = OFF;
 }
 void Automata::coin(int sum) {
-   if (state == WAIT || state == ACCEPT) {
-      state = ACCEPT;
-      cash = cash + sum;
-   }
+  if (state == WAIT || state == ACCEPT) {
+    state = ACCEPT;
+    cash = cash + sum;
+  }
 }
 void Automata::getMenu() {
-   for (int i = 0; i < menu.capacity(); i++) {
-      std::cout << i + 1 << ") " << menu[i] << " : " << prices[i] << "р\n";
-   }
+  for (int i = 0; i < menu.capacity(); i++) {
+    std::cout << i + 1 << ") " << menu[i] << " : " << prices[i] << "р\n";
+  }
 }
 Automata::State Automata::getState() { 
-   return state; 
+  return state; 
 }
 void Automata::choice(int num) {
-   if (state == ACCEPT) {
-      if (num < 0 || num >  menu.capacity()) {
-         std::cout << "Неправильно выбран напиток\n";
-      } else {
-         state = CHECK;
-         check(num);
-      }
-   }
+  if (state == ACCEPT) {
+    if (num < 0 || num >  menu.capacity()) {
+      std::cout << "Неправильно выбран напиток\n";
+    } else {
+      state = CHECK;
+      check(num);
+    }
+  }
 }
 void Automata::check(int num) {
-   if (state == CHECK) {
-      if (cash < prices[num]) {
-         std::cout << "Недостаточно денег\n";
-         state = WAIT;
-      } else {
-         cook(num);
-      }
-   }
+  if (state == CHECK) {
+    if (cash < prices[num]) {
+      std::cout << "Недостаточно денег\n";
+      state = WAIT;
+    } else {
+      cook(num);
+    }
+  }
 }
 void Automata::cancel() {
-   if (state == ACCEPT || state == CHECK) state = WAIT;
+  if (state == ACCEPT || state == CHECK) state = WAIT;
 }
 void Automata::cook(int num) {
-   if (state == CHECK) {
-      state = COOK;
-      std::cout << "Приготовление напитка...\n";
-      sleep(longitudes[num]);
-      finish(cash - prices[num]);
-   }
+  if (state == CHECK) {
+    state = COOK;
+    std::cout << "Приготовление напитка...\n";
+    sleep(longitudes[num]);
+    finish(cash - prices[num]);
+  }
 }
 void Automata::finish(int rest) { 
-   std::cout << "Напиток готов\n";
-   if (rest > 0) std::cout << "Сдача: " << rest << "\n";
-   cash = 0;
-   state = WAIT; 
+  std::cout << "Напиток готов\n";
+  if (rest > 0) std::cout << "Сдача: " << rest << "\n";
+  cash = 0;
+  state = WAIT; 
 }
