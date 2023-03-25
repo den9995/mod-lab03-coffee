57fac1b0996f6542edf2811e90a2c7a8caae01a6
den9995 <den9995@gmail.com>
2023-03-25 22:00:38 +0300

    indents

--- include/Automata.h
+++ include/Automata.h
@@ -4,34 +4,38 @@
 #include <vector>
 #include <string>
 class Automata {
-   public:
-     enum State {
-       OFF,
-       WAIT,
-       ACCEPT,
-       CHECK,
-       COOK
-     };
-   private:
-     int cash;
-     std::vector<std::string> menu;
-     std::vector<int> prices;
-     std::vector<int> longitudes;
-     State state;
-     int menuEntry;
-   public:
-     Automata();
-     void on();
-     void off();
-     void coin(int);
-     void getMenu();
-     State getState();
-     void choice(int);
-     void cancel();
-   private:
-      void check(int);
-      void cook(int);
-      void finish(int);
+
+ public:
+   enum State {
+     OFF,
+     WAIT,
+     ACCEPT,
+     CHECK,
+     COOK
+   };
+
+ private:
+   int cash;
+   std::vector<std::string> menu;
+   std::vector<int> prices;
+   std::vector<int> longitudes;
+   State state;
+   int menuEntry;
+
+ public:
+   Automata();
+   void on();
+   void off();
+   void coin(int);
+   void getMenu();
+   State getState();
+   void choice(int);
+   void cancel();
+
+ private:
+    void check(int);
+    void cook(int);
+    void finish(int);
 };
 
 #endif  // INCLUDE_AUTOMATA_H_
